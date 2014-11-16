ea88731784b252cabdbb3c3dffdea0ab59050602
Steve Kinney <hello@stevekinney.net>
2014-11-16 10:21:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var WebSocketServer = require('ws').Server;
+var socket = new WebSocketServer({ port: 8080 });
+
+socket.broadcast = function(data) {
+  this.clients.forEach(function (client) {
+    client.send(data);
+  });
+};
+
+socket.on('connection', function(connection) {
+  connection.on('message', function(message) {
+    console.log('Received: %s', message);
+    socket.broadcast(message);
+  });
+});
